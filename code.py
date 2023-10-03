92c36bf3822a73b78b35b5d029e58ac31f71914b
kikilor99 <kristelor@yahoo.fr>
2023-10-03 09:13:03 -0500

    the first function of python file

---
+++ code.py
@@ -0,0 +1,21 @@
+def greetings():
+    name = input("entrez votre nom: ")
+    print("Hello "+ name) 
+
+def ismajor(age):
+    if age >= 18:
+        return True
+    else : 
+        return False
+
+
+
+
+def main():
+    greetings()
+    age = int(input("Entrez votre age: " ))
+    if ismajor(age):
+        print("Bienvenue dans le programme!")
+    else :
+        print("Desole, mais vous n'avez pas l'age requis!")
+main()
